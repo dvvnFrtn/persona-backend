79f46105b0e425068c23670411599f2309845968
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-05-14 14:44:47 +0700

    chore: init cors configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const { memberRouter } = require('./members/member.router');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(memberRouter);
 
 app.get('/api/members/pic/:img', (req, res) => {
