85ca508f1b32afa0753ab85a8f3aedef92afb488
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-05-14 15:34:44 +0700

    fix: cors configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const { memberRouter } = require('./members/member.router');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET");
+  
+    if (req.method === "OPTIONS") return res.status(200).json({});
+  
+    next();
+  });
+
 app.use(memberRouter);
 
 app.get('/api/members/pic/:img', (req, res) => {
